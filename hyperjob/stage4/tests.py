457273d127678bd5dce4b4ebbde907a56f004b22
ant1k9 <ant1k9@protonmail.com>
2020-01-30 23:36:10 +0300

    Aggregate all tests to one file

--- hyperjob/stage4/tests.py
+++ hyperjob/stage4/tests.py
@@ -1,173 +1,22 @@
 # -*- coding: utf-8 -*-
-import http.cookiejar
-import re
-import urllib
-import sqlite3
-
-from hstest.django_test import DjangoTest, TEST_DATABASE
-from hstest.check_result import CheckResult
 from hstest.test_case import TestCase
 
-INITIAL_USERS = [
-    (1, 'Lemon_2059', 'contemp2015@protonmail.com', True),
-    (2, 'RuthlessnessSirens-1882', 'oversured1842@yahoo.com', True),
-    (3, 'moping_1935', 'tenons1970@outlook.com', True),
-    (4, 'MillagePenstemon-1843', 'chrisman1923@yandex.com', True),
-    (5, 'Archeus.1930', 'concentric1895@gmail.com', True),
-    (6, 'BenzalazineCurite.1832', 'quassiin1927@live.com', True),
-    (7, 'Bossa-1831', 'breena1977@live.com', False),
-    (8, 'ClinkChinho_2027', 'adansonia1808@gmail.com', False),
-    (9, 'RepassableTournefortian.1973', 'vomer1822@yahoo.com', False),
-    (10, 'debenture-1898', 'average2014@yahoo.com', False),
-]
+from .base import HyperJobTest
 
-INITIAL_VACANCIES = [
-    (1, 'Botanist'),
-    (2, 'Signwriter'),
-    (3, 'Stewardess'),
-    (4, 'Medical Secretary'),
-    (5, 'Stone Cutter'),
-    (6, 'Musician'),
-]
-
-INITIAL_RESUMES = [
-    (7, 'Charge Hand'),
-    (8, 'Occupations'),
-    (9, 'Milklady'),
-    (10, 'Auctioneer'),
-]
-
-
-class ModelsTestMixin(object):
-    def check_create_vacancies(self) -> CheckResult:
-        connection = sqlite3.connect(TEST_DATABASE)
-        cursor = connection.cursor()
-        try:
-            cursor.executemany(
-                'INSERT INTO auth_user '
-                '(`id`, `username`, `email`, `is_staff`, `password`, `is_superuser`, '
-                '`first_name`, `last_name`, `is_active`, `date_joined`) '
-                'VALUES (?, ?, ?, ?, "", false, "", "", true, datetime())',
-                INITIAL_USERS[:len(INITIAL_VACANCIES)]
-            )
-            cursor.executemany(
-                'INSERT INTO vacancy_vacancy (`author_id`, `description`) VALUES (?, ?)',
-                INITIAL_VACANCIES
-            )
-            connection.commit()
-
-            cursor.execute('SELECT `author_id`, `description` FROM vacancy_vacancy')
-            result = cursor.fetchall()
-
-            for item in INITIAL_VACANCIES:
-                if item not in result:
-                    return CheckResult.false('Check your Vacancy model')
-            return CheckResult.true()
-
-        except sqlite3.DatabaseError as err:
-            return CheckResult.false(str(err))
-
-    def check_create_resumes(self) -> CheckResult:
-        connection = sqlite3.connect(TEST_DATABASE)
-        cursor = connection.cursor()
-        try:
-            cursor.executemany(
-                'INSERT INTO auth_user '
-                '(`id`, `username`, `email`, `is_staff`, `password`, `is_superuser`, '
-                '`first_name`, `last_name`, `is_active`, `date_joined`) '
-                'VALUES (?, ?, ?, ?, "", false, "", "", true, datetime())',
-                INITIAL_USERS[len(INITIAL_VACANCIES):]
-            )
-            cursor.executemany(
-                'INSERT INTO resume_resume (`author_id`, `description`) VALUES (?, ?)',
-                INITIAL_RESUMES
-            )
-            connection.commit()
-
-            cursor.execute('SELECT `author_id`, `description` FROM resume_resume')
-            result = cursor.fetchall()
-
-            for item in INITIAL_RESUMES:
-                if item not in result:
-                    return CheckResult.false('Check your Resume model')
-            return CheckResult.true()
-
-        except sqlite3.DatabaseError as err:
-            return CheckResult.false(str(err))
-
-
-class HyperJobLoginTest(DjangoTest, ModelsTestMixin):
-    USERNAME = 'Sparrow_1949'
-    PASSWORD = 's<myW8Dh'
-
-    def check_signup(self) -> CheckResult:
-        opener = urllib.request.build_opener(
-            urllib.request.HTTPCookieProcessor(self.cookie_jar)
-        )
-        try:
-            response = opener.open(f'http://localhost:{self.port}/signup')
-        except urllib.error.URLError:
-            return CheckResult.false('Cannot connect to the signup page.')
-
-        csrf_options = re.findall(
-            b'<input[^>]+value="(?P<csrf>\w+)"[^>]*>', response.read()
-        )
-        if not csrf_options:
-            return CheckResult.false('Missing csrf_token in the form')
-
-        try:
-            response = opener.open(
-                f'http://localhost:{self.port}/signup',
-                data=urllib.parse.urlencode({
-                    'csrfmiddlewaretoken': csrf_options[0],
-                    'username': self.USERNAME,
-                    'password1': self.PASSWORD,
-                    'password2': self.PASSWORD,
-                }).encode()
-            )
-            if f'login' in response.url:
-                return CheckResult.true()
-            return CheckResult.false('Cannot signup: problems with form')
-        except urllib.error.URLError as err:
-            return CheckResult.false(f'Cannot signup: {err.reason}')
-
-    def check_login(self) -> CheckResult:
-        opener = urllib.request.build_opener(
-            urllib.request.HTTPCookieProcessor(self.cookie_jar)
-        )
-        try:
-            response = opener.open(f'http://localhost:{self.port}/login')
-        except urllib.error.URLError:
-            return CheckResult.false('Cannot connect to the login page.')
-
-        csrf_options = re.findall(
-            b'<input[^>]+value="(?P<csrf>\w+)"[^>]*>', response.read()
-        )
-        if not csrf_options:
-            return CheckResult.false('Missing csrf_token in the form')
-
-        try:
-            response = opener.open(
-                f'http://localhost:{self.port}/login',
-                data=urllib.parse.urlencode({
-                    'csrfmiddlewaretoken': csrf_options[0],
-                    'username': self.USERNAME,
-                    'password': self.PASSWORD,
-                }).encode()
-            )
-            if 'login' not in response.url:
-                return CheckResult.true()
-            return CheckResult.false('Cannot login: problems with form')
-        except urllib.error.URLError as err:
-            return CheckResult.false(f'Cannot login: {err.reason}')
-
+class HyperJobTestRunner(HyperJobTest):
     def generate(self):
-        self.cookie_jar = http.cookiejar.CookieJar()
-
         return [
+            # 1 task
             TestCase(attach=self.check_server),
             TestCase(attach=self.check_create_vacancies),
             TestCase(attach=self.check_create_resumes),
+            # 2 task
+            TestCase(attach=self.check_greeting),
+            TestCase(attach=self.check_links),
+            # 3 task
+            TestCase(attach=self.check_vacancies),
+            TestCase(attach=self.check_resumes),
+            # 4 task
             TestCase(attach=self.check_signup),
             TestCase(attach=self.check_login),
         ]
@@ -177,4 +26,4 @@ class HyperJobLoginTest(DjangoTest, ModelsTestMixin):
 
 
 if __name__ == '__main__':
-    HyperJobItemsPageTest('hyperjob.manage').run_tests()
+    HyperJobTestRunner('hyperjob.manage').run_tests()
